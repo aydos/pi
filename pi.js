de203d70559ac3632e64a2eaa6460c9ebb36a143
Fahri Aydos <aydoscom@gmail.com>
2016-03-11 00:35:40 +0200

    worker control

--- pi.js
+++ pi.js
@@ -3,37 +3,34 @@ pi.js
 Copyright 2011-2016 Fahri Aydos
 http://aydos.com/pi
 */
-importScripts("decimal.min.js")
+importScripts("decimal.js")
 var pi = new Decimal(0) // calculated pi
 var pr = 0 //precision
+var ic = 0 //iteration count
 var real //decimal part of real pi
 this.onmessage = function(e) {
-	pr = e.data.prec;
+	pr = new Decimal(e.data.pr).toNumber()
+	ic = e.data.ic
 	Decimal.config({precision:pr+5})
 	real = Decimal.acos(-1).toString().substr(2)
-	importScripts(e.data.method+".js")
+	importScripts(e.data.s+".js")
 	pi_calc()
 }
-
 function pi_calc() {
-	for (var i = 0; i < 10000; i++) {
+	for (var i=0;i<=ic;i++) {
 		pi_calculate()
 		var p = pi.toString()
 		var k = pi_compare(p)
+		this.postMessage({p:p,i:i,k:k})
 		if (k==pr) {
-			this.postMessage({p:p,i:i,k:k})
-			this.close()
-		} else {
-			this.postMessage({p:p,i:i,k:k})
+			break;
 		}
 	}
 }
-
 function pi_compare(p) {
 	if (!p.startsWith("3.")) {
 		return 0
 	}
-	console.log(p)
 	p = p.substr(2)
 	for (var i=0;i<pr;i++) {
 		if (p[i] != real[i]) {
