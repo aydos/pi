30f77d4ff1966e0ddc455ac3079e64c5207d710d
Fahri Aydos <aydoscom@gmail.com>
2016-05-08 11:21:15 +0300

    mj updated

--- mj.js
+++ mj.js
@@ -3,8 +3,11 @@ mj.js
 Copyright 2011-2016 Fahri Aydos
 http://aydos.com/pi
 */
-function Decimal() {}
-importScripts("decimal.min.js")
+function Decimal() {
+	this.div = function() {}
+	this.sqrt = function() {}
+	return this
+}
 this.onmessage=function(e) {
 	importScripts(e.data.se+".js")
 	mj = pi_math()
